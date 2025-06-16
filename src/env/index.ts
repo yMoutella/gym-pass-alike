9fecbc1fe00bccf3511cc58284589ae553a437a3
Yure Moutella <ymoutella@gmail.com>
2025-06-16 15:41:33 -0300

    relationships created

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(["dev", "prd", "test"]).default("dev"),
-
-});
+    });
 
 const _env = envSchema.safeParse(process.env);
 if (!_env.success) {
